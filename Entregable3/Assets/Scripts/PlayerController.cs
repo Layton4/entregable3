669f827381bdd98732aac7a7837a5ca2ce79ec36
Layton4 <Javierete4774@gmail.com>
2021-10-27 19:07:54 +0200

    entrega por revisar

---
+++ Entregable3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.localScale += 2 * Vector3.one;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.rotation *= Quaternion.Euler(45, 0, 45);
+        }
+    }
+
+    public void movementRotation(KeyCode key, Vector3 axes)
+    {
+        if (Input.GetKeyDown(key))
+        {
+        transform.rotation *= Quaternion.Euler(axes);
+        }
+    }
+}
